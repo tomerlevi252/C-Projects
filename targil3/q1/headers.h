8033b21850d5d1bda3a9944e907e05f0ab953642
Tomer Levi <tomerlevi252@gmail.com>
2025-08-19 23:54:54 +0300

    Delete targil3 directory

--- targil3/q1/headers.h
+++
@@ -1,7 +0,0 @@
-#define MAX_GEN 11
-#define MAX_ROW 10
-#define MAX_COLUMN 10
-
-void change(int a[][MAX_ROW][MAX_COLUMN], int gen, int row, int column,int nrow,int ncolumn);
-int countNeighbors(int a[][MAX_ROW][MAX_COLUMN],int gen,int row,int column,int nrow,int ncolumn);
-void printArray(int a[][MAX_ROW][MAX_COLUMN],int gen,int row,int column);
--- targil3/q2/headers.h
+++
@@ -1,3 +0,0 @@
-void printBits(unsigned int num);
-unsigned int loopBits(unsigned int a,int n);
-unsigned int ChangeCharArrayToNum(char str[]);
